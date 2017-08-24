5c6884e92700f22e2a026ad5d8e2ed6a4707a449
ellertr <ellertr@gmail.com>
2017-08-24 20:30:14 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,18 +20,8 @@
       if (!name.value || !email.value || !message.value) {
         alertify.error('Please check your entries');
         return false;
-      } else {
-        $.ajax({
-          method: 'POST',
-          url: '//formspree.io/ellert@ellertrunars.com',
-          data: $('#contact-form').serialize(),
-          datatype: 'json'
-        });
-        e.preventDefault();
-        $(this).get(0).reset();
-        return alertify.success('Message sent');
-      }
+      } 
     });
   });
 
-}).call(this);
+})();
